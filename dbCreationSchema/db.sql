647500568357638f41c110ea97e6c8812e785d14
dariofo2 <alejandrodariofo2@gmail.com>
2024-12-16 09:41:54 +0100

    add DB

---
+++ dbCreationSchema/db.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE IF NOT EXISTS banker;
+USE banker;
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS accounts (
+    id INT NOT NULL AUTO_INCREMENT,
+    userid INT NOT NULL,
+    name VARCHAR(20) NOT NULL,
+    type VARCHAR(20) NOT NULL,
+    balance INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (userid) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS movements (
+    id INT NOT NULL AUTO_INCREMENT,
+    origin_account_id INT NOT NULL,
+    destination_account_id INT NOT NULL,
+    money INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (origin_account_id) REFERENCES accounts(id),
+    FOREIGN KEY (destination_account_id) REFERENCES accounts(id)
+);
